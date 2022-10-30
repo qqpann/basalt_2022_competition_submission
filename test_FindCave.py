01f66a9512bce3adb3a3ca13420c780bacd4af26
qqhann <qiu.gits@gmail.com>
2022-10-30 15:59:57 +0900

    add: VPT

--- test_FindCave.py
+++ test_FindCave.py
@@ -1,14 +1,18 @@
 import logging
-import coloredlogs
+import pickle
 
 import aicrowd_gym
+import coloredlogs
 import minerl
+import torch
 
+from agent import MineRLAgent
 from config import EVAL_EPISODES, EVAL_MAX_STEPS
 
 coloredlogs.install(logging.DEBUG)
 
 MINERL_GYM_ENV = 'MineRLBasaltFindCave-v0'
+
 
 
 def main():
@@ -19,6 +23,12 @@ def main():
     # Load your model here
     # NOTE: The trained parameters must be inside "train" directory!
     # model = None
+    agent_parameters = pickle.load(open("data/foundation-model-3x.model", "rb"))
+    policy_kwargs = agent_parameters["model"]["args"]["net"]["args"]
+    pi_head_kwargs = agent_parameters["model"]["args"]["pi_head_opts"]
+    pi_head_kwargs["temperature"] = float(pi_head_kwargs["temperature"])
+    agent = MineRLAgent(env, policy_kwargs=policy_kwargs, pi_head_kwargs=pi_head_kwargs, device="cuda" if torch.cuda.is_available() else "cpu")
+    agent.load_weights("data/foundation-model-3x.weights")
 
     for i in range(EVAL_EPISODES):
         obs = env.reset()
@@ -28,14 +38,10 @@ def main():
             # Step your model here.
             # Currently, it's doing random actions
             # for 200 steps before quitting the episode
-            random_act = env.action_space.sample()
+            # random_act = env.action_space.sample()
+            act = agent.get_action(obs)
 
-            if step_counter < 200:
-                random_act["ESC"] = 0
-            else:
-                random_act["ESC"] = 1
-
-            obs, reward, done, info = env.step(random_act)
+            obs, reward, done, info = env.step(act)
 
             if done:
                 break
